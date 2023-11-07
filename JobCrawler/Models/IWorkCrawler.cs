bcea30c2fac2ee89197b5c858fd7560a53e000f9
rb <rkrb@tyfonmail.se>
2023-11-07 20:21:08 +0100

    interface for crawler model

---
+++ JobCrawler/Models/IWorkCrawler.cs
@@ -0,0 +1,9 @@
+namespace JobCrawler.Models;
+
+public interface IWorkCrawler
+{
+    public string Title { get; set; }
+    public string Location { get; set; }
+    public string Keywords { get; set; }
+    public string Batch { get; set; }
+}
