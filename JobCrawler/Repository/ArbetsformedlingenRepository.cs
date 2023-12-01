187aed44b88952d7ab9507c40caec33731c6e61e
rb <rkrb@tyfonmail.se>
2023-12-01 20:57:58 +0100

    StaleElementWrapper class added

--- JobCrawler/Repository/ArbetsformedlingenRepository.cs
+++ JobCrawler/Repository/ArbetsformedlingenRepository.cs
@@ -64,6 +64,40 @@ public class ArbetsformedlingenRepository : IWebRepository
         return jobs;
     }
 
+
+
+    private class StaleElementWrapper
+    {
+        private By locator;
+        private IWebElement element;
+        private IWebDriver driver;
+
+        public StaleElementWrapper(IWebDriver driver, By locator)
+        {
+            this.driver = driver;
+            this.locator = locator;
+            this.element = driver.FindElement(locator);
+        }
+
+        public void Click()
+        {
+            
+          
+            try
+            {
+    
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", element);
+               element.Click();
+            }
+            catch (StaleElementReferenceException)
+            {
+                element = driver.FindElement(locator);
+                element.Click();
+            }
+        }
+    }
+
+
     public List<string> AcquireInterestingJobs(string keywords)
     {
         Task.Delay(TimeSpan.FromSeconds(5)).Wait();
